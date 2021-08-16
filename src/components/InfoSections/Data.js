c94666b38a962ff1a79836330157142ff09d5f97
abhiram11 <abhiramsatpute@gmail.com>
2021-08-16 17:27:00 +0530

    done with below info sections, now services CARDS

--- src/components/InfoSections/Data.js
+++ src/components/InfoSections/Data.js
@@ -18,39 +18,39 @@ export const homeObjOne = {
 };
 
 export const homeObjTwo = {
-  id: "about",
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: "Premium Bank",
-  headline: "Unlimited Transactions with Zero Fees",
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Log into your account at any time",
   description:
-    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees",
-  buttonLabel: "Get Started",
-  imgStart: false,
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.",
+  buttonLabel: "Learn More",
+  imgStart: true,
   img: require("../../images/svg-1.svg"),
-  alt: "Car",
-  dark: true,
+  alt: "Piggy Bank",
+  dark: false,
   //for buttons
-  primary: true,
-  darkText: false,
+  primary: false,
+  darkText: true,
 };
 
 export const homeObjThree = {
   id: "about",
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: "Premium Bank",
-  headline: "Unlimited Transactions with Zero Fees",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headline: "Creating an Account is Extremely Easy",
   description:
-    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees",
-  buttonLabel: "Get Started",
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
   imgStart: false,
   img: require("../../images/svg-1.svg"),
-  alt: "Car",
-  dark: true,
+  alt: "Paper",
+  dark: false,
   //for buttons
-  primary: true,
-  darkText: false,
+  primary: false,
+  darkText: true,
 };
