9a3f77aeca1a8b7d5f337aa5a9422ac0a530c79e
abhiram11 <abhiramsatpute@gmail.com>
2021-08-17 16:53:17 +0530

    finalized the sidebar/hamburger menu

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -41,6 +41,7 @@ export const SidebarMenu = styled.ul`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: repeat(6, 80px);
+
   text-align: center;
 
   @media screen and (max-width: 480px) {
@@ -49,22 +50,21 @@ export const SidebarMenu = styled.ul`
 `;
 
 export const SidebarLink = styled(LinkS)`
-display:flex;
-align-items: center;
-justify-content: center:
-font-size: 1.5rem;
-text-decoration: none;
-list-style: none;
-transition: 0.2s ease-in-out;
-text-decoration: none;
-color: #fff;
-cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
 
-&:hover {
+  &:hover {
     color: #01bf71;
     transition: 0.2s ease-in-out;
-}
-
+  }
 `;
 
 export const SideBtnWrap = styled.div`
